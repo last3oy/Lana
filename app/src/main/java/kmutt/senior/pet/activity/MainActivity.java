48f46b8afa8d349e75c71159d6054581d626f6c2
last3oy <softwarez_gelion@hotmail.com>
2016-01-26 18:34:32 +0700

    no message

---
+++ app/src/main/java/kmutt/senior/pet/activity/MainActivity.java
@@ -0,0 +1,128 @@
+package kmutt.senior.pet.activity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import kmutt.senior.pet.R;
+import kmutt.senior.pet.fragment.FragmentTemplate;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initInstances();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, FragmentTemplate.newInstance())
+                    .commit();
+        }
+    }
+
+    private void initInstances() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this
+                , drawerLayout
+                , R.string.hello_world
+                , R.string.hello_world
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MainBus.getInstance().register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainBus.getInstance().unregister(this);
+    }
+
+    @Subscribe
+    public void busEventReceived(BusEventDessert event){
+        DessertListManager.getInstance().setSelectedDao(event.getDao());
+
+        // TODO : Check Mobile/Tablet
+
+
+        FrameLayout moreInfoFrameLayout = (FrameLayout) findViewById(R.id.moreContainer);
+        if(moreInfoFrameLayout == null){
+            //mobile
+            Intent intent = new Intent(MainActivity.this,MoreInfoActivity.class);
+            startActivity(intent);
+        }else {
+            //tablet
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentContainer, MoreInfoFragment.newInstance())
+                    .commit();
+        }
+
+        //ทำให้เป็น paselable แล้วส่ง extra ไป
+    }*/
+}
---
+++ app/src/main/java/kmutt/senior/pet/fragment/FragmentTemplate.java
@@ -0,0 +1,66 @@
+package kmutt.senior.pet.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kmutt.senior.pet.R;
+
+/**
+ * Created by last3oy on 26/01/2016.
+ */
+public class FragmentTemplate extends Fragment {
+
+    public FragmentTemplate() {
+        super();
+    }
+
+    public static FragmentTemplate newInstance() {
+        FragmentTemplate fragment = new FragmentTemplate();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        initInstances(rootView);
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
+        // init instance with rootView.findViewById here
+        //setRetainInstance(true);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
