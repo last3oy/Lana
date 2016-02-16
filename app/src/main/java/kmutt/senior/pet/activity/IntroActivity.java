44ec9ec70137aac1cd707045da02396f1eae02e7
last3oy <softwarez_gelion@hotmail.com>
2016-02-16 14:22:50 +0700

    intro activity almost complete

---
+++ app/src/main/java/kmutt/senior/pet/activity/IntroActivity.java
@@ -0,0 +1,15 @@
+package kmutt.senior.pet.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kmutt.senior.pet.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
