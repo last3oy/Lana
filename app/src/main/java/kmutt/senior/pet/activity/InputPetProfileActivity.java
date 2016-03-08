84916deb151135eefd81b642e505176e655a61e0
last3oy <softwarez_gelion@hotmail.com>
2016-03-08 11:41:03 +0700

    rename input activity

---
+++ app/src/main/java/kmutt/senior/pet/activity/InputPetProfileActivity.java
@@ -0,0 +1,15 @@
+package kmutt.senior.pet.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kmutt.senior.pet.R;
+
+public class InputPetProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_pet_profile);
+    }
+}
--- app/src/main/java/kmutt/senior/pet/activity/WelcomeActivity.java
+++ app/src/main/java/kmutt/senior/pet/activity/WelcomeActivity.java
@@ -1,29 +1,23 @@
 package kmutt.senior.pet.activity;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
-
-import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 
 import kmutt.senior.pet.R;
 
-public class IntroActivity extends AppCompatActivity {
-    private static final String[] COUNTRIES = new String[] {
+public class WelcomeActivity extends AppCompatActivity {
+
+    /*private static final String[] COUNTRIES = new String[] {
             "Belgium", "France", "Italy", "Germany", "Spain"
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inputprofile);
+        setContentView(R.layout.activity_welcome);
 
-        CircleImageView t = (CircleImageView)findViewById(R.id.picture);
+        /*CircleImageView t = (CircleImageView)findViewById(R.id.picture);
         t.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,7 +32,7 @@ public class IntroActivity extends AppCompatActivity {
         MaterialBetterSpinner  textView = (MaterialBetterSpinner)
                 findViewById(R.id.spinner);
 
-        textView.setAdapter(adapter);
+        textView.setAdapter(adapter);*/
     }
 
 
