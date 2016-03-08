cc0e1a02856fcf6d305925c54fea5f39cdeb8c7a
last3oy <softwarez_gelion@hotmail.com>
2016-03-08 11:27:08 +0700

    intro xml full

--- app/src/main/java/kmutt/senior/pet/activity/IntroActivity.java
+++ app/src/main/java/kmutt/senior/pet/activity/IntroActivity.java
@@ -1,15 +1,47 @@
 package kmutt.senior.pet.activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 import kmutt.senior.pet.R;
 
 public class IntroActivity extends AppCompatActivity {
+    private static final String[] COUNTRIES = new String[] {
+            "Belgium", "France", "Italy", "Germany", "Spain"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_inputprofile);
+
+        CircleImageView t = (CircleImageView)findViewById(R.id.picture);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(IntroActivity.this,
+                        "The favorite list would appear on clicking this icon",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        MaterialBetterSpinner  textView = (MaterialBetterSpinner)
+                findViewById(R.id.spinner);
+
+        textView.setAdapter(adapter);
     }
+
+
+
+
 }
